3f7e56416afc0c3f6a6aaa7582743e7b153871db
mmendell <105883493+mmendell@users.noreply.github.com>
2022-10-14 13:21:43 -0400

    task 4.3

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
 
 class CitySearch extends Component {
+  state = {
+    query: "",
+    suggestions: [],
+  };
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({
+      query: value,
+      suggestions,
+    });
+  };
+
+  handleItemChanged = (suggestion) => {
+    this.setState({
+      query: suggestion
+    });
+  }
+
   render() {
     return (
       <div className="CitySearch">
-        <input type="text" className="city" />
-        <ul className="suggestions"></ul>
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
+        <ul className="suggestions">
+          {this.state.suggestions.map((suggestion) => (
+            <li key={suggestion}
+            onClick={() => this.handleItemChanged(suggestion)}
+            >{suggestion}</li>
+          ))}
+          <li key="all">
+            <b>see all</b>
+          </li>
+        </ul>
       </div>
     );
   }
--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,15 +1,66 @@
 import React from "react";
 import { shallow } from "enzyme";
 import CitySearch from "../CitySearch";
+import mockData from '../mock-data';
+import { extractLocations } from '../api'; 
 
 describe("<CitySearch /> componenet", () => {
+  let locations, CitySearchWrapper;
+  beforeAll(() => {
+    locations = extractLocations(mockData)
+    CitySearchWrapper = shallow(<CitySearch />);
+  });
+
   test("render city input", () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find(".city")).toHaveLength(1);
   });
 
   test("render  a lsit of suggestions", () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
+  });
+
+  test("renders text input properly", () => {
+    const query = CitySearchWrapper.state("query");
+    expect(CitySearchWrapper.find(".city").prop("value")).toBe(query);
+  });
+
+  test("change state when text input changes", () => {
+    CitySearchWrapper.setState({
+      query: "boston",
+    });
+    const eventObject = { target: { value: "new york" } };
+    CitySearchWrapper.find(".city").simulate("change", eventObject);
+    expect(CitySearchWrapper.state("query")).toBe("boston");
+  });
+
+  test('render list of suggestions correctly', () => {
+    const locations = extractLocations(mockData);
+    CitySearchWrapper.setState({ suggestions: locations});
+    const suggestions = CitySearchWrapper.state('suggestions');
+    expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
+    for (let i = 0; i < suggestions.length; i += 1) {
+      expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
+    }
+  });
+
+  test('suggestion list match the query when changed', () => {
+    CitySearchWrapper.setState({ query: '', suggestions: [] });
+    CitySearchWrapper.find(".city").simulate("change", {
+      target: { value: 'boston'}
+    });
+    const query = CitySearchWrapper.state("query");
+    const filteredLocations = locations.filter((location) => {
+      return location.toUpperCase().indexOF(query.toUpperCase()) > -1;
+    });
+    expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations);
+  })
+
+  test('selecting a suggestion should change the query state', () => {
+    CitySearchWrapper.setState({
+      query: "boston"
+    });
+    const suggestions = CitySearchWrapper.state('suggestions');
+    CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+    expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
   });
 });
