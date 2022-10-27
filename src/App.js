0aa01ca25d0f28a4402f1180f9a31cbb70de3d19
mmendell <105883493+mmendell@users.noreply.github.com>
2022-10-27 16:00:09 -0400

    4.7 fixes

--- src/App.js
+++ src/App.js
@@ -10,25 +10,39 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
+    numberOfEvents: 32,
+    locationSelected: 'all',
   };
 
-  updateEvents = (location) => {
+  updateEvents = (location, maxNumEvents) => {
+    if (maxNumEvents === undefined) {
+      maxNumEvents = this.state.numberOfEvents;
+    } else this.setState({ numberOfEvents: maxNumEvents });
+    if (location === undefined) {
+      location = this.state.locationSelected;
+    }
     getEvents().then((events) => {
-      const locationEvents =
+      let locationEvents =
         location === "all"
           ? events
           : events.filter((event) => event.location === location);
       this.setState({
-        events: locationEvents,
+        events: locationEvents.slice(0, maxNumEvents),
+        numberOfEvents: maxNumEvents,
+        locationSelected: location,
       });
     });
   };
 
-  componentDidMount() {
+
+ async componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({ 
+          events: events.slice(0, this.state.numberOfEvents),
+          locations: extractLocations(events),
+        });
       }
     });
   }
@@ -45,7 +59,7 @@ class App extends Component {
           updateEvents={this.updateEvents}
         />
         <NumberOfEvents
-          numberOfEvents={this.state.numberOfEvents}
+          numberOfEvents={this.state.events}
           updateEvents={this.updateEvents}
         />
         <EventList events={this.state.events} />
