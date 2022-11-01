4fa852ab0834fac9e74b7b2e03e405a8cbc68505
mmendell <105883493+mmendell@users.noreply.github.com>
2022-11-01 18:16:25 -0400

    fixing pie chart

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ class App extends Component {
         />
 
         <div className="data-vis-wrapper">
-          <EventGenre events={this.state.events} />
+          <EventGenre events={events} />
           <ResponsiveContainer height={400}>
             <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
               <CartesianGrid />
